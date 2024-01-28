d206d7431a8dd9e06d08fdd623b52ea77c972f75
Sokhonn Raksmeidaravid <156156144+RaksmeidaravidSOKHONN@users.noreply.github.com>
2024-01-28 16:04:38 +0700

    Update process-string.py
    Done

--- process-string.py
+++ process-string.py
@@ -6,6 +6,17 @@
 # * "Hello World" -> "HHeelllloo  WWoorrlldd"
 # * "1234!_ "     -> "11223344!!__  "
 
+#Code
+user_input = input("Enter the words: ")
+user_input1 = list(user_input)
+user_input2 = list(user_input)
+
+user_input1_2 = [None]*(len(user_input1)+len(user_input2))
+user_input1_2[::2] = user_input1
+user_input1_2[1::2] = user_input2
+
+user_str = ''. join(user_input1_2)
+print(user_str)
 
 # Application 2
 #  Given a string indicating a range of letters, return a string which includes all the letters in that range, including the last letter. Note that if the range is given in capital letters, return the string in capitals also!
@@ -17,8 +28,17 @@
 # "J-J" ➞ "J"
 # Notes A hyphen will separate the two letters in the string.
 
-
+#Code
 alphabet = "abcdefghijklmnopqrstuvwxyz"
-user_range = input("Enter a range of letters (e.g., a-z): ")
+upper_alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+Com_al_up = alphabet + upper_alphabet
 
+user_input = input("Enter a range of letters (e.g. a-z): ")
+user_split = user_input.split("-")
+first_letter = user_split[0]
+second_letter = user_split[1]
 
+first_index = Com_al_up.index(first_letter)
+second_index = Com_al_up.index(second_letter)
+result = Com_al_up[first_index: second_index +1]
+print(result)
